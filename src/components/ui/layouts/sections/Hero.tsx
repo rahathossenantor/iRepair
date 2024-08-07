a0c39d04f8b25d25312f782bf1600a2aac12017d
rahathossenantor <md.rahathossenantor@gmail.com>
2024-08-07 20:37:01 +0600

    Hero section initial animation implemented

--- src/components/ui/layouts/sections/Hero.tsx
+++ src/components/ui/layouts/sections/Hero.tsx
@@ -1,20 +1,74 @@
 import { Button } from "../../button";
 import macbookExposed from "../../../../assets/macbook-exposed.png";
+import { motion } from "framer-motion";
+
+// animations
+const intro = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.25,
+            delayChildren: 0.5
+        }
+    }
+};
+const introChildren = {
+    hidden: { opacity: 0, y: -200 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.5,
+            type: "spring",
+            bounce: 0.5
+        }
+    }
+};
 
 const Hero = () => {
     return (
         <div className="custom-container h-[calc(100vh-110px)] grid grid-cols-1 lg:grid-cols-2 place-items-center my-5">
-            <div>
-                <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-nowrap">
+            <motion.div
+                variants={intro}
+                initial="hidden"
+                animate="visible"
+            >
+                <motion.h1
+                    variants={introChildren}
+                    className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-nowrap"
+                >
                     <span className="text-gray">Don't worry,</span><br />
                     <span>We'll fix it.</span>
-                </h1>
-                <p className="text-dark-gray text-lg mb-5 mt-5 lg:mt-10">Welcome to <span className="text-primary-foreground font-semibold">iRepair</span>, your one-stop place for all kinds of <span className="text-primary-foreground font-semibold">Macbook</span> repairs and diagnostics.</p>
-                <Button>Book a service</Button>
-            </div>
-            <div className="w-3/4 lg:w-full mx-auto">
-                <img src={macbookExposed} className="-rotate-[35deg] md:h-[60%] lg:h-[90%] object-contain" alt="MacBook Exposed" />
-            </div>
+                </motion.h1>
+                <motion.p
+                    variants={introChildren}
+                    className="text-dark-gray text-lg mb-5 mt-5 lg:mt-10"
+                >Welcome to <span className="text-primary-foreground font-semibold">iRepair</span>, your one-stop place for all kinds of <span className="text-primary-foreground font-semibold">Macbook</span> repairs and diagnostics.</motion.p>
+                <motion.div
+                    variants={introChildren}
+                >
+                    <Button>Book a service</Button>
+                </motion.div>
+            </motion.div>
+            <motion.div
+            className="w-3/4 lg:w-full mx-auto"
+            initial={{
+                y: 0
+            }}
+            animate={{
+                y: 20,
+                transition: {
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    duration: 2,
+                    ease: "easeInOut"
+                }
+            }}
+            >
+                <img src={macbookExposed} className="-rotate-[35deg] object-contain" alt="MacBook Exposed" />
+            </motion.div>
         </div>
     );
 };
